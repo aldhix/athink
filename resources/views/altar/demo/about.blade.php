cb81fa2010cbd8f2cb2dc276f7ad56a2fec4147f
Aldhi Xar <aldhi.x@gmail.com>
2020-08-30 00:24:29 +0700

    update package

--- resources/views/altar/demo/about.blade.php
+++ resources/views/altar/demo/about.blade.php
@@ -6,7 +6,8 @@
 <div class="row">
 	<div class="col">
 		<p>
-			<strong>AdminLTE 3 Bootstrap 4.5 Laravel 7</strong> Paket component AdminLTE yang di conversi kedalam bentuk blade Laravel. Github : <a href="https://github.com/aldhix/adminlte3-bootstrap4-laravel7">https://github.com/aldhix/adminlte3-bootstrap4-laravel7</a>
+			<strong>Altar</strong> : AdminLTE 3 dengan Component Laravel 7. <br>
+			Github : <a href="https://github.com/aldhix/altar" target="_blank">https://github.com/aldhix/altar</a>
 		</p>
 	</div>
 </div>
